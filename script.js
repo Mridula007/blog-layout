c4c40c2e5b20df9fc6081c21e29433bff694c0cc
Mridula007 <mridulasasikumar30@gmail.com>
2025-04-22 19:42:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function toggleDarkMode() {
-  document.body.classList.toggle('dark');
+  document.body.classList.toggle('dark-mode');
+
+  const mode = document.body.classList.contains('dark-mode') ? 'dark' : 'light';
+  localStorage.setItem('theme', mode);
 }
+
+window.addEventListener('DOMContentLoaded', () => {
+  const savedTheme = localStorage.getItem('theme');
+  if (savedTheme === 'dark') {
+    document.body.classList.add('dark-mode');
+  }
+});
